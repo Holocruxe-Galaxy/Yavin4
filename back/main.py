3d1b55c7e810c33d3cd0d760266ec31216938e8e
Fernando <fernandocazon2@gmail.com>
2023-11-29 14:39:50 -0300

    feat language conversion

--- back/main.py
+++ back/main.py
@@ -2,9 +2,25 @@ from fuzzywuzzy import process
 import sqlite3
 from langchain.llms import OpenAI
 from dotenv import load_dotenv
+from googletrans import Translator, LANGUAGES
 
-
+translator = Translator()
 load_dotenv()
+
+
+def translate_text(text, src_language="auto", dest_language="en"):
+    try:
+        # Instead of raising an error, let's just print a warning and proceed
+        if dest_language not in LANGUAGES.values():
+            print(f"Warning: Unsupported language code: {dest_language}. Proceeding with translation anyway.")
+        
+        translation = translator.translate(text, src=src_language, dest=dest_language)
+        print(f"Translation: '{text}' from {src_language} to {dest_language} -> '{translation.text}'")  # Log the translation
+        return translation.text
+    except Exception as e:
+        print(f"Error in translation: {e}")
+        return f"Translation error: {e}"
+
 
 
 def get_closest_match(query, choices, limit=1):
@@ -91,27 +107,65 @@ def chat_with_assistant(txt_filepath):
 
 def generate_response(user_input, txt_filepath='data.txt'):
     try:
+        # Define the target language for processing (English)
+        target_language = 'en'
+
+        print(f"Original Input: {user_input}")  # Log the original input
+
+        # Detect the language of the input
+        detected_language_result = translator.detect(user_input)
+        source_language = detected_language_result.lang
+
+        print(f"Detected Language: {source_language}")  # Log the detected language
+
+        # Translate to English if the source language is not English
+        if source_language != target_language:
+            translated_input = translate_text(user_input, src_language=source_language, dest_language=target_language)
+            print(f"Translated Input (to English): {translated_input}")  # Log the translated input
+        else:
+            translated_input = user_input
+        print(f"Translated Input (to English): {translated_input}")  # Log the translated input
+
         llm = OpenAI(temperature=0.5)
         with sqlite3.connect('personal_assistant.db') as conn:
             cursor = conn.cursor()
             txt_qa_pairs = load_txt_data(txt_filepath)
-            txt_response = get_answer(cursor, user_input, txt_qa_pairs)
+
+            # Check for an answer in the text data
+            txt_response = get_answer(cursor, translated_input, txt_qa_pairs)
             if txt_response:
-                print("Bot (from TXT):", txt_response)
-                return txt_response
+                # Translate back to the original language if needed
+                translated_response = translate_text(txt_response, dest_language=source_language) if source_language != target_language else txt_response
+                print(f"Response from TXT: {translated_response}")  # Log the response from TXT data
+                return translated_response
 
-            db_response = get_from_db(cursor, user_input)
+            # Check for an answer in the database
+            db_response = get_from_db(cursor, translated_input)
             if db_response:
-                print("Bot (from DB):", db_response)  
-                return db_response
+                translated_response = translate_text(db_response, dest_language=source_language) if source_language != target_language else db_response
+                print(f"Response from DB: {translated_response}")  # Log the response from the database
+                return translated_response
 
-            response = llm(user_input)
-            print("Bot (from LLM):", response)
-            store_in_db(conn, cursor, user_input, response)
-            return response
+            # If not found, use the LLM
+            response = llm(translated_input)
+            store_in_db(conn, cursor, translated_input, response)
+            translated_response = translate_text(response, dest_language=source_language) if source_language != target_language else response
+            print(f"Response from LLM: {translated_response}")  # Log the response from LLM
+            return translated_response
+
     except Exception as e:
         print(f"Error in generate_response: {e}")
         return str(e)
+
+
+
+    except Exception as e:
+        print(f"Error in generate_response: {e}")
+        return str(e)
+
+
+
+
 
 
 if __name__ == "__main__":
