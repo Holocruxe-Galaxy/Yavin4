3566e281e59cc54879aa53f187379bee741667a8
Facundo Fierro <77708145+facufierro@users.noreply.github.com>
2024-08-07 19:29:11 +0200

    resetting the repository

--- front/src/components/message.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Message = ({ sender, text }) => (
-  <div className={`message ${sender}`}>
-    {sender === "user" ? "You: " : "HoloChat: "}{text}
-  </div>
-);
-
-export default Message;
